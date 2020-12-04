e643349a183777b544bff473cb836b7a06ffb860
IsaAlane <isamelo.d@gmail.com>
2020-12-04 12:31:30 -0300

    Conclusão Tela Define Local

--- Android/defineLocal/app/src/main/java/com/ifmg/definelocal/MainActivity.java
+++ Android/defineLocal/app/src/main/java/com/ifmg/definelocal/MainActivity.java
@@ -2,12 +2,14 @@ package com.ifmg.definelocal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Calendar;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,9 +29,27 @@ public class MainActivity extends AppCompatActivity {
         numero = (TextView) findViewById(R.id.numTxt);
         cep = (TextView) findViewById(R.id.cepTxt);
 
-        //localizarBtn = (Button) findViewById(R.id.localizarBtn);
+        localizarBtn = (Button) findViewById(R.id.localizarBtn);
 
 
+        /*
+        Vector<TextView> camposTexto = new Vector<>();
+        camposTexto.add(cidade);
+        camposTexto.add(estado);
+        camposTexto.add(rua);
+        camposTexto.add(numero);
+        camposTexto.add(cep);
 
+        defineTexto(camposTexto);
+        */
+
+    }
+
+
+    private void defineTexto(Vector<TextView> vec){
+        for(TextView v: vec){
+            Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/Poppins-Regular.ttf");
+            v.setTypeface(tf);
+        }
     }
 }
