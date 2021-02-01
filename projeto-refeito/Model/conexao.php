b5f8c8762c6687f554feed88e9c8cfd91b8ea1b9
IsaAlane <isamelo.d@gmail.com>
2021-02-01 20:54:12 -0300

    problems

--- projeto-refeito/Model/conexao.php
+++ projeto-refeito/Model/conexao.php
@@ -1,21 +1,20 @@
 <?php
-<<<<<<< HEAD
-    
-    //parâmetros de acesso ao BD
-=======
->>>>>>> 9cf1bbedc1547f6e1c75755896d07c49c882a595
     $servidor = "200.18.128.52";
     $usuario = "caio_isabella";
     $senha = "caio_isabella";
     $nomeBD = "caio_isabella";
     $con = new mysqli($servidor, $usuario, $senha, $nomeBD);
-<<<<<<< HEAD
 
 
     //conferindo conexão com o servidor
     if($con->connect_error){
         die("Falha na conexão".$con->connect_error);
     }
+
+<<<<<<< HEAD
+
+
+    
 =======
 >>>>>>> 9cf1bbedc1547f6e1c75755896d07c49c882a595
 ?>
