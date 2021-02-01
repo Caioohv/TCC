b6a5ace8c059449d1a154f7c4a50bc8b6c265f2a
IsaAlane <isamelo.d@gmail.com>
2021-02-01 20:50:19 -0300

    alteração na conexao

---
+++ projeto-refeito/Model/conexao.php
@@ -0,0 +1,15 @@
+<?php
+    
+    //parâmetros de acesso ao BD
+    $servidor = "200.18.128.52";
+    $usuario = "caio_isabella";
+    $senha = "caio_isabella";
+    $nomeBD = "caio_isabella";
+    $con = new mysqli($servidor, $usuario, $senha, $nomeBD);
+
+
+    //conferindo conexão com o servidor
+    if($con->connect_error){
+        die("Falha na conexão".$con->connect_error);
+    }
+?>
