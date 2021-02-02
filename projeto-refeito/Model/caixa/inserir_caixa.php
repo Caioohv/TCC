ad2e137d2a48aeed68848f9289504d7213328090
Caio <caiohv@protonmail.com>
2021-02-01 23:26:08 -0300

    mano oq ta acontecendo

---
+++ projeto-refeito/Model/caixa/inserir_caixa.php
@@ -0,0 +1,36 @@
+<!-- <form action="Model/caixa/inserir_caixa.php" method="POST" name="fdmCadastro" class="form-cad-caixa">
+
+            <p>Dados Básicos</p>
+            <input type="text" name="Nome" id="Nome" class="caixaTexto" placeholder="Nome">
+            
+            <select name="status" id="status" class="caixaTexto">
+                <option value="Ativo">Ativo</option>
+                <option value="Manutenção">Manutenção</option>
+                <option value="Inativo">Inativo</option>
+                <option value="Indisponivel">Indisponível</option>
+            </select>
+
+            <p>Endereço</p>
+            <input type="text" name="Cidade" id="Cidade" class="caixaTexto" placeholder="Cidade">
+            <input type="text" name="Bairro" id="Bairro" class="caixaTexto" placeholder="Bairro">
+            <br>
+            <input type="text" name="Rua"    id="Rua"    class="caixaTexto" placeholder="Rua">
+            <input type="text" name="Numero" id="Numero" class="caixaTexto" placeholder="Numero">
+            <br>
+            <input type="text" name="Estado" id="Estado" class="caixaTexto" placeholder="Estado">
+            <input type="text" name="Pais"   id="Pais"   class="caixaTexto" placeholder="País">
+            <br>
+            
+
+            <div class="btn">
+                <input type="submit" name="submit" value="Cadastrar" class="btn-cad-caixa">
+            </div>
+            
+        </form> -->
+
+<?php 
+
+    
+
+
+?>
