5ac1863f9ac297f792e20aba730a5434f2192e45
Caio <caiohv@protonmail.com>
2021-02-02 10:15:03 -0300

    excluir 2

--- projeto-refeito/View/Includes/Caixa/excluir.php
+++ projeto-refeito/View/Includes/Caixa/excluir.php
@@ -13,11 +13,14 @@
             if($con -> connect_error){
                 die("Falha na conexão com o banco de dados: " . $con->connection_error);
             }
-            $sql = 'DELETE * FROM caixa WHERE id_caixa';
-            
+            $sql = 'DELETE * FROM caixa WHERE id_caixa = '.$_GET['id'];
+            //echo $sql;
             $result = mysqli_query($con, $sql);
             
-            $n = mysqli_fetch_assoc($result);
+            if($result){
+                echo '<p>Excluido com sucesso</p>';
+                echo '<?php header(location=index.php?page=visualizarC ?>';
+            }
 
             $_SESSION['id-c'] = $_GET['id'];
             ?>
