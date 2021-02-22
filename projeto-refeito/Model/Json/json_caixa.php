d62547eb2caa9c7907f4a89784a683a526ca6f20
IsaAlane <isamelo.d@gmail.com>
2021-02-22 14:33:48 -0300

    criando arquivo json

--- projeto-refeito/Model/Json/json_caixa.php
+++ projeto-refeito/Model/Json/json_caixa.php
@@ -7,21 +7,21 @@ include_once 'Model/conexao.php';
         die("Falha na conexão com o banco de dados: " . $con->connection_error);
     }
 
+
     $sql = "SELECT * FROM caixa";
     $result = mysqli_query($con, $sql);
-
+    $arrayCaixas = array();
     if($result->num_rows > 0){
         while($row = $result -> fetch_assoc()){
-            $objJson->cidade = $row['cidade'];
-            $objJson->bairro = $row['bairro'];
-            $objJson->rua = $row['rua'];
-            $objJson->numero = $row['numero'];
-            $objJson->estado = $row['estado'];
-            $objJson->pais = $row['pais'];
-    
-            $objJson = json_encode($objJson);
-    
-            echo $objJson;
+            array_push($arrayCaixas, array("Latitute"=>$row["latitude"], "Longetude"=>$row["longetude"]));
+           
+        }
+        $objJson = json_encode($arrayCaixas);
+
+        if(file_put_contents("caixas.json", $objJson)){
+            echo 'Arquivo gerado com sucesso';
+        }else{
+            echo 'Erro ao gerar arquivo';
         }
 }
 
