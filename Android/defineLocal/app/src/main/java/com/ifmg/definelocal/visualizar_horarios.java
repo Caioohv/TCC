339bd3bf7a71905e1bb921edd67ab561b41f8623
Caio <caiohv@protonmail.com>
2021-02-22 16:42:16 -0300

    Apagando os trem antigo

--- Android/defineLocal/app/src/main/java/com/ifmg/definelocal/visualizar_horarios.java
+++
@@ -1,5 +0,0 @@
-package com.ifmg.definelocal;
-
-public class visualizar_horarios {
-
-}
--- src/Model/conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-    
-    //parâmetros de acesso ao BD
-    $servidor = "200.18.128.52";
-    $usuario = "caio_isabella";
-    $senha = "caio_isabella";
-    $nomeBD = "caio_isabella";
-    $con = new mysqli($servidor, $usuario, $senha, $nomeBD);
-?>
